ea24edcb9f129d94bd40010ded23cb27e9468979
Eunicegenel <eunel_soliveres@yahoo.com>
2021-05-16 23:40:36 +0800

    added update path

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,2 @@
-setTimeout(function(){ document.getElementById('preloader').style.opacity = 0 }, 3000);
+setTimeout(function(){ document.getElementById('preloader').style.opacity = 0; 
+setTimeout(function(){ document.getElementById('preloader').style.display = 'none' }, 1000);}, 3000);
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/games' => 'games#index'
   get '/games/new' => 'games#new'
-  post '/games' => 'games#create', as: 'game_create'
+  post'/games' => 'games#create', as: 'game_create'
+  get '/games/update' => 'games#update'
+  post'/games' => 'games#update', as: 'game_update'  
 end
