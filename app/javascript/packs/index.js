7dcf7f0220a6e60aa553312df7497c503f0fc40a
Eunicegenel <eunel_soliveres@yahoo.com>
2021-05-18 16:28:50 +0800

    finished index

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,2 +1,21 @@
+document.getElementById("tooltip-Btn").addEventListener("click", function() {
+  document.getElementById('tooltipModal').style.display = 'flex';
+  document.getElementById('close_modal').style.display = 'block';
+});
+
+document.getElementById("close_modal").addEventListener("click", function() {
+  document.getElementById('tooltipModal').style.display = 'none';
+  document.getElementById('close_modal').style.display = 'none';
+  document.getElementById('modalTitle').innerHTML = 'Game Meter Tooltip';
+  document.getElementById('actionBtn').style.display = 'block';
+  document.getElementById('createForm').style.display = 'none';
+});
+
+document.getElementById("createGame").addEventListener("click", function() {
+  document.getElementById('modalTitle').innerHTML = 'ADD NEW GAME';
+  document.getElementById('actionBtn').style.display = 'none';
+  document.getElementById('createForm').style.display = 'flex';
+}); 
+
 setTimeout(function(){ document.getElementById('preloader').style.opacity = 0; 
-setTimeout(function(){ document.getElementById('preloader').style.display = 'none' }, 1000);}, 3000);
+setTimeout(function(){ document.getElementById('preloader').style.display = 'none'; }, 1000);}, 3000);
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
 
   get '/games' => 'games#index'
-  get '/games/new' => 'games#new'
   post'/games' => 'games#create', as: 'game_create'
   get '/games/details' => 'games#details'
   post'/games/details' => 'games#comment', as: 'game_comment'
